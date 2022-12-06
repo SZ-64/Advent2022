6a75e53c11434a296b00365ba8bf94e87ae70394
SZ-64 <50932675+SZ-64@users.noreply.github.com>
2022-12-06 03:49:13 -0500

    Added day 6

--- Code/6-12-22.py
+++ Code/6-12-22.py
@@ -3,12 +3,40 @@ import Utils
 
 # Comments
 def part_one():
-    pass
+    window = []
+    line = Utils.read_file_as_lines("Input\\6-12-22.txt")[0]
+    for index in range(len(line)):
+        window.append(line[index])
+
+        if len(window) < 4:
+            continue
+
+        window_set = set(window)
+
+        if len(window_set) == 4:
+            print(index + 1)
+            return
+
+        window.pop(0)
 
 
 # Comments
 def part_two():
-    pass
+    window = []
+    line = Utils.read_file_as_lines("Input\\6-12-22.txt")[0]
+    for index in range(len(line)):
+        window.append(line[index])
+
+        if len(window) < 14:
+            continue
+
+        window_set = set(window)
+
+        if len(window_set) == 14:
+            print(index + 1)
+            return
+
+        window.pop(0)
         
 
 def main():
