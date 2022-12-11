525e7af101554ce25e310d98ab4aec9a27dffe07
SZ-64 <50932675+SZ-64@users.noreply.github.com>
2022-12-11 10:39:06 -0500

    Finally got day 9 worked out!

--- Code/9-12-22.py
+++ Code/9-12-22.py
@@ -2,7 +2,7 @@ import Utils
 from Classes.Node import Node
 
 
-rope_knots = 9
+rope_knots = 10
 visited_set = set()
 
 def tick(head_ref, tail_ref, direction):
@@ -10,13 +10,19 @@ def tick(head_ref, tail_ref, direction):
         move_rope(head_ref, direction)
     
     if abs(head_ref.x - tail_ref.x) > 1:
-        move_rope(tail_ref, direction)
         if head_ref.y > tail_ref.y:
             move_rope(tail_ref, "U")
         elif head_ref.y < tail_ref.y:
             move_rope(tail_ref, "D")
+        if head_ref.x > tail_ref.x:
+            move_rope(tail_ref, "R")
+        elif head_ref.x < tail_ref.x:
+            move_rope(tail_ref, "L")
     elif abs(head_ref.y - tail_ref.y) > 1:
-        move_rope(tail_ref, direction)
+        if head_ref.y > tail_ref.y:
+            move_rope(tail_ref, "U")
+        elif head_ref.y < tail_ref.y:
+            move_rope(tail_ref, "D")
         if head_ref.x > tail_ref.x:
             move_rope(tail_ref, "R")
         elif head_ref.x < tail_ref.x:
@@ -24,7 +30,6 @@ def tick(head_ref, tail_ref, direction):
 
     #print(f"Move {head_ref.id} to ({head_ref.x}, {head_ref.y}), {tail_ref.id} to ({tail_ref.x}, {tail_ref.y})")
     if tail_ref.id == "Tail":
-        print(f"{tail_ref.id} added to set ({tail_ref.x}, {tail_ref.y})")
         visited_set.add(f"({tail_ref.x}, {tail_ref.y})")
 
 
@@ -74,12 +79,9 @@ def part_two():
         direction = tokens[0]
         steps = int(tokens[1])
 
-        print(move)
         for _ in range(steps):
             for knot in range(1, len(rope)):
                 tick(rope[knot - 1], rope[knot], direction)
-            print()
-        print()
     
     print(visited_set)
     print(len(visited_set))
